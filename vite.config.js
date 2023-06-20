619d4cd671d589f655bd24c2b2a96cb6cd824619
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-06-20 17:03:42 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'Tic_Tac_Toe_REACT',
+  base: 'https://keven-bardales.github.io/Tic_Tac_Toe_REACT',
 });
